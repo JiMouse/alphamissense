3d032c79a649ab493721a2486fa15bf8d692cf86
Jean-Marie Ravel <33331141+JiMouse@users.noreply.github.com>
2023-09-29 16:21:47 +0200

    Update queryGene.py

--- queryGene.py
+++ queryGene.py
@@ -8,45 +8,53 @@
 import sys
 from queryFile import queryAlphaMissense
 
+def queryGene(gene):
+    # Specify path to file
+    file_path = 'ucsc_ncbiRefSeqSelect_hg19.txt'
+
+    # Initialize a list to store the matching lines
+    matching_lines = []
+
+    # Open the TSV file for reading
+    with open(file_path, 'r') as file:
+        # get header
+        header = file.readline().strip('\n')
+
+        # define column position
+        column_gene = header.split('\t').index('name2')
+        column_chrom = header.split('\t').index('chrom')
+        column_txStart = header.split('\t').index('txStart')
+        column_txEnd = header.split('\t').index('txEnd')
+        
+        # Iterate through each line in the file
+        for line in file:
+            # Split the line into columns using the tab character as the separator
+            columns = line.strip().split('\t')
+
+            if gene == columns[column_gene]:
+                # If it does, add the entire line to the matching_lines list
+                matching_lines = line.strip().replace('chr','').split('\t')
+                matching_lines
+                result = list( matching_lines[i] for i in [column_chrom, column_txStart, column_txEnd]) #column_gene
+                # [column_gene, column_chrom, column_txStart, column_txEnd]
+                continue
+
+    if matching_lines == []:
+        print('no results')
+    else:
+        # Print the matching lines
+        # print("hg19 coordinates")
+        # print(['column_gene', 'column_chrom', 'column_txStart', 'column_txEnd'])
+        # print(result)
+
+        queryAlphaMissense(result[0], result[1], result[2])
+
 # define variables
 gene = sys.argv[1]
+queryGene(gene)
 
-# Specify path to file
-file_path = 'ucsc_ncbiRefSeqSelect_hg19.txt'
-
-# Initialize a list to store the matching lines
-matching_lines = []
-
-# Open the TSV file for reading
-with open(file_path, 'r') as file:
-    # get header
-    header = file.readline().strip('\n')
-
-    # define column position
-    column_gene = header.split('\t').index('name2')
-    column_chrom = header.split('\t').index('chrom')
-    column_txStart = header.split('\t').index('txStart')
-    column_txEnd = header.split('\t').index('txEnd')
-    
-    # Iterate through each line in the file
-    for line in file:
-        # Split the line into columns using the tab character as the separator
-        columns = line.strip().split('\t')
-
-        if gene == columns[column_gene]:
-            # If it does, add the entire line to the matching_lines list
-            matching_lines = line.strip().replace('chr','').split('\t')
-            matching_lines
-            result = list( matching_lines[i] for i in [column_chrom, column_txStart, column_txEnd]) #column_gene
-            # [column_gene, column_chrom, column_txStart, column_txEnd]
-            continue
-
-if matching_lines == []:
-    print('no results')
-else:
-    # Print the matching lines
-    print("hg19 coordinates")
-    # print(['column_gene', 'column_chrom', 'column_txStart', 'column_txEnd'])
-    # print(result)
-
-    queryAlphaMissense(result[0], result[1], result[2])
+# with list
+# genes = ['MECP2', 'MEF2C', 'CDKL5', 'IQSEC2', 'FOXG1'] 
+# for gene in genes:
+#     print(gene)
+#     queryGene(gene)
