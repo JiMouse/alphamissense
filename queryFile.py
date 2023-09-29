278fd0c6a80cb4dcce8cb82178f0e65600f685c1
Jean-Marie Ravel <33331141+JiMouse@users.noreply.github.com>
2023-09-29 09:12:36 +0200

    Update queryFile.py

--- queryFile.py
+++ queryFile.py
@@ -1,20 +1,18 @@
-# to look for a given variation. The idea is to used the previously split files (one file per chromosomes)
+# Use to look for a given genomic position or a genomic range (in the same chromosome). The idea is to used the previously split files (one file per chromosomes).
 
 # usage
 # with Anaconda PowerShell Prompt
 # cd C:\Users\u637660\Documents\AlphaMissense
-# python queryFile.py chr pos
 # python queryFile.py 1 34330247
+# python queryFile.py 1 34330247 34330250
 
 import sys
 
-# Define the string you want to search for in column 1
-# chr = '1'
-# pos = '34330247' #POS
-
-# or using sys
+# define variables
 chr = sys.argv[1]
-pos = sys.argv[2]
+pos1 = sys.argv[2]
+if len(sys.argv) > 3:
+    pos2 = sys.argv[3]
 
 # Specify path to file
 file_path = 'chr'+ chr + '.txt'
@@ -32,15 +30,20 @@ with open(file_path, 'r') as file:
         # Split the line into columns using the tab character as the separator
         columns = line.strip().split('\t')
 
-        # Check if the second column contains the search string : exact match == | partial match in 
-        if pos == columns[1]:
-            # If it does, add the entire line to the matching_lines list
-            matching_lines.append(line.strip())
+        if len(sys.argv) == 3:
+            # Check if the second column contains the search string : exact match == | partial match in 
+            if pos1 == columns[1]:
+                # If it does, add the entire line to the matching_lines list
+                matching_lines.append(line.strip())
+        else:
+            if columns[1] >= pos1 and columns[1] <= pos2:
+                matching_lines.append(line.strip())
 
 if matching_lines == []:
     print('no results')
 else:
     # Print the matching lines
+    print("hg19 coordinates")
     print(first_line) #header
     for matching_line in matching_lines:
         print(matching_line)
